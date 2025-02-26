af83dfc0c073ec4123a4e81a41a5ece216320f9d
Thunder Oyebi <thunderoyebi@my.unt.edu>
2025-02-25 19:39:11 -0600

    Changed each item in number pad to be single characters for simpler display. Added calculator screen and edges.

--- main.cpp
+++ main.cpp
@@ -4,19 +4,31 @@ using namespace std;
 int main()
 {
     //Holds calculator "buttons"
-    string displayArr[1][4] = {
-        {"C", "()", "%", "/"},
+    string displayArr[5][4] = {
+        {"C", "P", "%", "/"},
+        {"7", "8", "9", "*"},
+        {"4", "5", "6", "-"},
+        {"1", "2", "3", "+"},
+        {"-", "0", ".", "="}
     };
 
     //Displays calculator
-    for (int row = 0; row < 1; row++)
+    //Lines 17-19 is the calulator screen
+    cout << "+------------+" << endl;
+    cout << "|" << "            " << "|" << endl;
+    cout << "+------------+" << endl;
+    //lines 21-31 is the number pad
+    for (int row = 0; row < 5; row++)
     {
+        cout << "|";
         for (int col = 0; col < 4; col++)
         {
-            cout << displayArr[row][col] << " ";
+            cout << " " << displayArr[row][col] << " ";
         }
+        cout << "|";
         cout << endl;
     }
+    cout << "+------------+" << endl;
 
     return 0;
 }
