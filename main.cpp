b93417c22c34f68ed2dbb1148067513d09e51793
Thunder Oyebi <thunderoyebi@my.unt.edu>
2025-02-25 19:29:29 -0600

    Made a whole number pad for the calculator.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    //Holds calculator "buttons"
+    string displayArr[1][4] = {
+        {"C", "()", "%", "/"},
+    };
+
+    //Displays calculator
+    for (int row = 0; row < 1; row++)
+    {
+        for (int col = 0; col < 4; col++)
+        {
+            cout << displayArr[row][col] << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
